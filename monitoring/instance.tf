282a3342b2c9eca72a20872496fb8c84b8c75070
AdewaleOpeloyeru <Opeloyeruadewale@icloud.com>
2025-10-18 00:35:25 +0100

    setup monitoring

--- monitoring/instance.tf
+++ monitoring/instance.tf
@@ -4,10 +4,9 @@ data "ibm_resource_group" "group" {
 
 module "monitoring" {
   source  = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version = "1.4.0"  # Use the latest version available
+  version = "1.9.0"  # Use the latest version available
 
   resource_group_id = data.ibm_resource_group.group.id
   region            = "eu-gb" # you can change this to your own region
-  name              = "deployment_monito"
   plan              = "lite"
 }
